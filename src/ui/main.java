bf7465b6ad5c927e90867b6343de403f95763210
Deric Dallaire <dallaire.deric@gmail.com>
2017-10-26 16:11:45 -0400

    Basic Hello World ui

---
+++ src/ui/main.java
@@ -0,0 +1,27 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class main extends JFrame {
+	public main() {
+		setBounds(100, 100, 965, 744);
+		JLabel lblHelloWorld = new JLabel("Hello World!");
+		getContentPane().add(lblHelloWorld, BorderLayout.NORTH);
+	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					main frame = new main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
