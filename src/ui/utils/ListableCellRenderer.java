59f4d50fccb475ddc2ddefde48cc836428b39666
Déric Dallaire <dallaire.deric@gmail.com>
2017-12-01 12:56:01 -0500

    Removed warning

--- src/ui/utils/ListableCellRenderer.java
+++ src/ui/utils/ListableCellRenderer.java
@@ -8,7 +8,10 @@ import javax.swing.JList;
 import data.IListable;
 
 public class ListableCellRenderer extends DefaultListCellRenderer {
-  public Component getListCellRendererComponent(JList list, Object value, int index,
+
+	private static final long serialVersionUID = 1L;
+
+public Component getListCellRendererComponent(JList<?> list, Object value, int index,
       boolean isSelected, boolean cellHasFocus) {
     super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
